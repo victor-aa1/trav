a054b0dad88ecce887e104215eef1a4cb1a74726
victor agbeo <victor@victorstudio.tech>
2023-05-25 13:54:35 -0500

    trav

---
+++ woflo/woflo.h
@@ -0,0 +1,18 @@
+//
+//  woflo.h
+//  woflo
+//
+//  Created by vicagbeo on 5/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for woflo.
+FOUNDATION_EXPORT double wofloVersionNumber;
+
+//! Project version string for woflo.
+FOUNDATION_EXPORT const unsigned char wofloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <woflo/PublicHeader.h>
+
+
